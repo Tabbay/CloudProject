233ebcf3aeeceaf11ea4d06c90c9804ec72c7d70
Tabby <tabiac4@gmail.com>
2019-04-18 11:04:04 -0400

    small changes to index page

---
+++ index.js
@@ -0,0 +1,10 @@
+function showsearchBox(){
+  
+  if(document.getElementById('searchBox').style.display == "block"){
+    document.getElementById('searchBox').style.display = "none";
+  }
+  else{
+    document.getElementById('searchBox').style.display = "block";
+  }
+
+}
